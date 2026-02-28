8a2ff16334c50bd3e6a66c762d2259a51c02abbc
Marius Ahmsus <marius.ahsmus@gmail.com>
2026-02-28 17:00:10 +0100

    feat: executionCtx

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import { Hono } from 'hono'
 import { bot } from "./bot.js"
-import { waitUntil } from '@vercel/functions'
 
 const app = new Hono()
 
 app.get("/", (c) => c.text("Bot is running"))
 
 app.post("/api/webhooks/telegram", async (c) => {
-    console.log("WEBHOOK HIT");
+    console.log("WEBHOOK HIT")
+    const handler = bot.webhooks.telegram
 
-    return bot.webhooks.telegram(c.req.raw, {
-        waitUntil: (task) =>
-            waitUntil(
-                task.catch((err) => {
-                    console.error("BACKGROUND ERROR", err);
-                })
-            )
+    if (!handler) {
+        return c.text("Telegram adapter not configured", 404)
+    }
+
+    return handler(c.req.raw, {
+        waitUntil: (task) => c.executionCtx.waitUntil(task)
     })
 })
 
